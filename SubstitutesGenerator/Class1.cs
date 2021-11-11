10972d87c41a2a3336216f3a98e16eca575550f5
Alberto Monteiro <alberto.monteiro@live.com>
2021-11-11 02:16:46 -0300

    Remove unnecessary code

--- SubstitutesGenerator/Class1.cs
+++ SubstitutesGenerator/Class1.cs
@@ -1,12 +1,17 @@
 ﻿namespace SubstitutesGenerator
 {
-    public interface IOla{}
-    public interface IHello{}
+    public interface IOla
+    {
+    }
+
+    public interface IHello
+    {
+    }
+
     public class Class1
     {
         private readonly IOla ola;
         private readonly IHello hello;
-
         public Class1(IOla ola, IHello hello)
         {
             this.ola = ola;
@@ -17,10 +22,11 @@
     class Class1Test
     {
         private Class1 _sut;
-
+        private readonly IOla _ola = default;
+        private readonly IHello _hello = default;
         public Class1Test()
         {
-            _sut = new Class1(default);
+            _sut = new Class1(_ola, _hello);
         }
     }
 }
